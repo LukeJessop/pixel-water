2908101dcc75b2bc45effffdcdca3a7630cefd66
Lucas Jessop <lucasjessop@MacBook-Air.localdomain>
2022-08-25 16:07:06 -0600

    created screen sizing -> pixel creation functionality w/ a little styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
 
 function App() {
-  function getWindowDimensions(){
-    const {innerWidth: width, innerHeight:height} = window
-    console.log('width ' + width, 'height ' + height)
-    return(
-      height,
-      width
-    )
-  }
-  console.log(getWindowDimensions())
-  return (
+  const {innerWidth: width, innerHeight: height} = window
+  
+  let pixelArr = []
+  let pixelSizer = 20
+  createPixels()
 
-    <div className="App">
-      
-    </div>
+
+  function createPixels(){
+    for(let x = 0; x < pixelSizer; x++){
+      for(let y = 0; y < pixelSizer; y++){
+        pixelArr.push({
+          xCoord: x,
+          yCoord: y
+        })
+      }
+    }
+  }
+
+
+
+  let pixelMap = pixelArr.map((e) => { //creates all of the "pixels" or squares on the screen (basically a prefab in a way)
+    return(
+      <div 
+        style={{height: pixelSizer, width: width/pixelSizer, height: height/pixelSizer}} 
+        className='pixel' 
+        id={`y${e.xCoord}`} 
+        key={`${e.xCoord}-${e.yCoord}`}
+      >
+
+      </div>
+    )
+  })
+  return (
+      <div className='pixel-container'>
+        {pixelMap}
+      </div>
   );
 }
 
